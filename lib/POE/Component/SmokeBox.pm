38edc7a62be6ef1ae1b7c10f9210a1879832bcb5
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-02-15 12:45:26 +0000

    Bump to dev release 0.43_01

--- lib/POE/Component/SmokeBox.pm
+++ lib/POE/Component/SmokeBox.pm
@@ -8,7 +8,7 @@ use POE::Component::SmokeBox::Job;
 use POE::Component::SmokeBox::Result;
 use vars qw($VERSION);
 
-$VERSION = '0.42';
+$VERSION = '0.43_01';
 
 sub spawn {
   my $package = shift;
--- lib/POE/Component/SmokeBox/Backend/Test/Idle.pm
+++ lib/POE/Component/SmokeBox/Backend/Test/Idle.pm
@@ -5,7 +5,7 @@ use warnings;
 use base qw(POE::Component::SmokeBox::Backend::Base);
 use vars qw($VERSION);
 
-$VERSION = '0.42';
+$VERSION = '0.43_01';
 
 sub _data {
   my $self = shift;
